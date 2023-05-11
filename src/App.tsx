8bdc2ea5d6f9fd2d9d26fa446e04a4f2138f5708
SergKo <zef1rqo@gmail.com>
2023-05-11 09:14:30 +0300

    Adaptive carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import CarouselBox from "./Components/CaruselBox";
+import CarouselBox from "./Components/CarouselBox";
 import CardsBoxAbout from "./Components/CardsBoxAbout";
 import CardBoxLights from "./Components/CardsBoxLights";
 import Footer from "./Components/Footer";
